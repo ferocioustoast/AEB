5fae181d85c0097139db77327aa9f967170e1bf7
ferocioustoast <ferocioustoast7@gmail.com>
2023-08-10 16:29:48 -0400

    testing ramping values

--- AEB.py
+++ AEB.py
@@ -17,14 +17,14 @@ rminvol = 0.4  # Right minimum volume: 0.4
 amp = 1  # Number used to multiply the sinewave by: 1
 
 ramp_up = True  # If at zero for inactive_time, ramp volume up over ramp_time
-ramp_time = 0.8  # Time, in seconds, to ramp volume up
+ramp_time = 0.3  # Time, in seconds, to ramp volume up
 ramp_inc = 20  # Number of steps to take when ramping, more adds more time
-inactive_time = 0.6  # Time, in seconds, to be at zero to trigger ramp up
+inactive_time = 0.1  # Time, in seconds, to be at zero to trigger ramp up
 
-ramp_down = True  # Ramp volume down over ramp_time
-ramp_time_d = 0.8  # Time, in seconds, to ramp volume down
+ramp_down = True  # Ramp volume down over ramp_time_d
+ramp_time_d = 0.3  # Time, in seconds, to ramp volume down
 ramp_inc_d = 20  # Number of steps to take when ramping, more adds more time
-inactive_time_d = 0.6  # Time, in seconds, to trigger ramp down
+inactive_time_d = 0.5  # Time, in seconds, to trigger ramp down
 
 half_way = False  # Old way, use half_rum to switch channels
 extended = False  # Used with half_way, keep lvol at lmaxvol after half_rum
